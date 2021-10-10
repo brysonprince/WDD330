60b27f041f1ba791769dcd6f2e72e115cf6169ef
brysonprince <59940182+brysonprince@users.noreply.github.com>
2021-10-09 19:39:22 -0600

    Got the week links working

--- js/main.js
+++ js/main.js
@@ -1,26 +1,27 @@
 const links = [
     {
-      label: "Week1",
-      url: "week1/index.html",
-      label: "Week2",
-      url: "week2/index.html"
+        label: "Week1",
+        url: "week1/index.html",
+    },
+    {
+        label: "Week2",
+        url: "week2/index.html"
     }
   ]
 
-let list = document.getElementsByTagName('ol')
+const ol = document.getElementsByTagName('ol')[0];
 
 function createLinks(){
-    newList = "";
-    for (let i = 0; i < list.length; i++){
-        newList += "<li>";
-        newList += "<a href=\"";
-        newList += links[i].url;
-        newList += "\">"
-        newList += links[i].label;
-        newList += "</a>";
-        newList += "</li>";
+    for (let i = 0; i < links.length; i++){
+        var li = document.createElement("li");
+        var a = document.createElement("a")
+
+        a.appendChild(document.createTextNode(links[i].label))
+        a.setAttribute("href", links[i].url)
+
+        li.appendChild(a);
+        ol.appendChild(li);
     }
-    list.innerHTML = newList;
 }
 
 createLinks()
