959801b1ff71676466d7d0ea2e1bbd0773a479f2
brysonprince <59940182+brysonprince@users.noreply.github.com>
2022-04-28 12:08:53 -0600

    updated quiz

--- week2/quiz/main.js
+++ week2/quiz/main.js
@@ -4,18 +4,32 @@ const quiz = [
     ["What is Batman's real name?","Bruce Wayne"]
 ];
 
-let score = 0
+function start(quiz){
+    let score = 0;
 
-alert('Welcome to Quiz Ninja!');
+    // main game loop
+    for(const [question,answer] of quiz){
+        const response = ask(question);
+        check(response,answer);
+    }
 
-for(const [question,answer] of quiz){
-    const response = prompt(question);
-    if(response === answer){
+    gameOver();
+
+    function ask(question){
+        return prompt(question);
+    }
+
+    function check(response,answer){
+        if(response === answer){
         alert('Correct!');
         score++;
-    } else {
+        } else {
         alert(`Wrong! The correct answer was ${answer}`);
+        }
+    }
+
+    function gameOver(){
+        alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
     }
 }
-
-alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+start(quiz);
