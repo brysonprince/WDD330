0c0cf0109a0f9140399e283849f8f8ab85e1ac51
brysonprince <59940182+brysonprince@users.noreply.github.com>
2022-04-28 11:49:43 -0600

    line spacing

--- week2/practice/chessboard.js
+++ week2/practice/chessboard.js
@@ -2,15 +2,15 @@ let size = 8;
 let board = '';
 
 for(let y = 1; y <= size; y++){
-  for(let x = 1; x <= size; x++){
-    if((y % 2 == 0 && x % 2) == 0 || (y % 2 != 0 && x % 2 != 0)){
-      board += '#';
+    for(let x = 1; x <= size; x++){
+      if((y % 2 == 0 && x % 2) == 0 || (y % 2 != 0 && x % 2 != 0)){
+        board += '#';
+      }
+      else{
+        board += ' ';
+      }
     }
-    else{
-      board += ' ';
-    }
-  }
-  board += '\n';
+    board += '\n';
 }
 
 console.log(board);
